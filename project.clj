c5390172c0d6898eb2ce56279d263e2b79a0e686
Wes Morgan <cap10morgan@gmail.com>
2015-10-14 14:27:09 -0600

    update dependencies

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject google-civic "1.0.1"
   :description "Google Civic Info API client"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.7.6"]
-                 [cheshire "5.2.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-http "2.0.0"]
+                 [cheshire "5.5.0"]])
