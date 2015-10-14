13db13c68fb6fa04fe0bd9716cd31a3f6a92a889
Wes Morgan <cap10morgan@gmail.com>
2015-10-14 14:36:51 -0600

    Version 2.0.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject google-civic "2.0.0"
+(defproject google-civic "2.0.1-SNAPSHOT"
   :description "Google Civic Info API client"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-http "2.0.0"]
