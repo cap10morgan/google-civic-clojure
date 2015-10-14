5620bcd18e5a2e766103ca9489c96c7136ce3be2
Wes Morgan <cap10morgan@gmail.com>
2015-10-14 14:28:04 -0600

    use v2 Civic Info API
    v1 has been deprecated and shut down

--- project.clj
+++ project.clj
@@ -2,4 +2,6 @@
   :description "Google Civic Info API client"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-http "2.0.0"]
-                 [cheshire "5.5.0"]])
+                 [cheshire "5.5.0"]
+                 [slingshot "0.12.2"]]
+  :profiles {:dev {:dependencies [[clj-http-fake "1.0.1"]]}})
--- src/google_civic/core.clj
+++ src/google_civic/core.clj
@@ -1,29 +1,40 @@
 (ns google-civic.core
-  (:require [clj-http.client :as client])
-  (:require [cheshire.core :as json]))
+  (:require [clj-http.client :as client]
+            [cheshire.core :as json]
+            [slingshot.slingshot :refer [try+]]))
 
-(def api-root "https://www.googleapis.com/civicinfo/us_v1")
+(def api-root "https://www.googleapis.com/civicinfo/v2")
 
-(defn- api-url [api-key endpoint]
-  (str api-root endpoint "?key=" api-key))
+(defn api-url [endpoint]
+  (str api-root endpoint))
 
-(defn- api-response [api-key endpoint & [post-data]]
-  (let [url (api-url api-key endpoint)]
-    (if post-data
-      (client/post url {:body (json/generate-string post-data)
-                        :content-type :json})
-      (client/get url {:accept :json}))))
+(defn api-response [api-key endpoint & [query-params]]
+  (let [url (api-url endpoint)]
+    (client/get url {:accept :json
+                     :query-params (merge {:key api-key} query-params)})))
 
-(defn- api-req
-  ([api-key endpoint & [post-data]]
-    (json/parse-string (:body (api-response api-key endpoint post-data)) true)))
+(defn api-req
+  ([api-key endpoint & [query-params]]
+   (try+
+     (let [response (api-response api-key endpoint query-params)]
+       (json/parse-string (:body response) true))
+     (catch Object response
+       (merge response {:body (json/parse-string (:body response) true)})))))
 
 (defn elections [api-key]
-  (:elections (api-req api-key "/elections")))
+  (let [response (api-req api-key "/elections")]
+    (if-let [elections (:elections response)]
+      elections
+      (:body response))))
 
-(defn voter-info [api-key election-id addr]
-  (let [post-data {:address addr}]
-    (select-keys
-      (api-req api-key (str "/voterinfo/" election-id "/lookup") post-data)
-      [:election :normalizedInput :state])))
-
+(defn voter-info [api-key addr & [election-id]]
+  (let [query-params {:address addr}
+        query-params (if election-id
+                       (merge query-params {:electionId election-id})
+                       query-params)
+        response (api-req api-key (str "/voterinfo") query-params)
+        voter-info (select-keys response
+                                [:election :normalizedInput :contests :state])]
+    (if (empty? voter-info)
+      (:body response)
+      voter-info)))
