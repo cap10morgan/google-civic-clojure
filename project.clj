feb64d38c279ed84b6b5a9ce17891384e2ecee91
Troy Hill <troy@democracy.works>
2016-10-31 08:38:01 -0600

    lein-release plugin: preparing 2.1.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject google-civic "2.1.0-SNAPSHOT"
+(defproject google-civic "2.1.0"
   :description "Google Civic Info API client"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-http "3.3.0"]
